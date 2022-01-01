56a172d4c8c5e4352362ea16b26af95b0da6a3b0
Pratyush Mendon <mendonpratyush908@gmail.com>
2022-01-01 18:07:21 +0530

    write function which displays array of playermoves in the display

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const gameBoard = (function() {
-let playerMoves = [0,1,2,3,4]
+let playerMoves = ['x','o','x','o','x','o','x','o','x']
 
 return {playerMoves}
 
 })()
 
 
+const displayController = (function () {
+    let listItems = document.getElementsByTagName('li')
+    gameBoard.playerMoves.forEach((item, index) => listItems[index].textContent = gameBoard.playerMoves[index])
+})()
+
+
+
 const gameController = (function () {
 
 })
                                             
-
-gameBoard.playerMoves[2] = "abcd"
-console.log(gameBoard.playerMoves)
