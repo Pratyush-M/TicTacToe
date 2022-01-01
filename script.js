a285201c866adfb510bf0cda1122eb5578f589b0
Pratyush Mendon <mendonpratyush908@gmail.com>
2022-01-01 15:43:18 +0530

    add html css and js files, add module pattern funciton to js

---
+++ script.js
@@ -0,0 +1,15 @@
+const gameBoard = (function() {
+let playerMoves = [0,1,2,3,4]
+
+return {playerMoves}
+
+})()
+
+
+const gameController = (function () {
+
+})
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+
+gameBoard.playerMoves[2] = "abcd"
+console.log(gameBoard.playerMoves)
