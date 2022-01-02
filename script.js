0594854078f298e5fae815ad47ae748245e38cc9
Pratyush Mendon <mendonpratyush908@gmail.com>
2022-01-02 20:04:07 +0530

    add condition in markSpot function to not overwrite an existing move

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (function() {
-let playerMoves = [null,'o',null,'o',null,'o','x','o','x']
+let playerMoves = [null,null,null,null,null,null,null,null,null]
 let listItems = document.getElementsByTagName('li')
 let placeMoves = function() {playerMoves.forEach((item, index) => listItems[index].textContent = playerMoves[index])}
 
@@ -29,9 +29,12 @@ const playerFactory = function (name) {
 const playerOne = (function () {
     let listItemArray = [...gameBoard.listItems]
     console.log(listItemArray)
-    let markSpot = listItemArray.forEach((item, index) => item.addEventListener("click", () => {gameBoard.playerMoves[index] = 'x';
-    gameBoard.placeMoves();
-    console.log(gameBoard.playerMoves);
-}))
-    return markSpot
+    let markSpot = function () {listItemArray.forEach((item, index) => item.addEventListener("click", () => {
+        if (!gameBoard.playerMoves[index]){
+            gameBoard.playerMoves[index] = 'x';
+            gameBoard.placeMoves();
+        }
+        else {return}
+        }))}
+    return markSpot()
 })()
