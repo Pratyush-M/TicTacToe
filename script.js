182496b4bbcb089ebd9932bfcfa7ab697e65a843
Pratyush Mendon <mendonpratyush908@gmail.com>
2022-01-02 19:49:16 +0530

    create function to play a move, add it to array and then display array, also move functions and variables from display controller to gameboard

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const gameBoard = (function() {
-let playerMoves = ['x','o','x','o','x','o','x','o','x']
+let playerMoves = [null,'o',null,'o',null,'o','x','o','x']
+let listItems = document.getElementsByTagName('li')
+let placeMoves = function() {playerMoves.forEach((item, index) => listItems[index].textContent = playerMoves[index])}
 
-return {playerMoves}
+return {playerMoves, listItems, placeMoves}
 
 })()
 
 
 const displayController = (function () {
-    let listItems = document.getElementsByTagName('li')
-    gameBoard.playerMoves.forEach((item, index) => listItems[index].textContent = gameBoard.playerMoves[index])
+    
 })()
 
 
@@ -17,3 +18,20 @@ const gameController = (function () {
 
 })
                                             
+const playerFactory = function (name) {
+    let playerInfo = {
+        playerOne: "praty",
+        playerTwo: "simba"
+    } 
+    return {playerInfo}
+}
+
+const playerOne = (function () {
+    let listItemArray = [...gameBoard.listItems]
+    console.log(listItemArray)
+    let markSpot = listItemArray.forEach((item, index) => item.addEventListener("click", () => {gameBoard.playerMoves[index] = 'x';
+    gameBoard.placeMoves();
+    console.log(gameBoard.playerMoves);
+}))
+    return markSpot
+})()
